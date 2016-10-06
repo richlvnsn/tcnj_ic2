8ae24faa5b0e076f6e97b4d2de2c23168ca3347a
DanSarnelli <sarneld1@tcnj.edu>
2016-10-05 22:36:46 -0400

    Edited description for our project, added inputs and outputs to Verilog file.

--- RAM/RAM.srcs/sources_1/new/RAM.v
+++ RAM/RAM.srcs/sources_1/new/RAM.v
@@ -6,10 +6,10 @@
 // Create Date: 10/05/2016 10:18:20 PM
 // Design Name: 
 // Module Name: RAM
-// Project Name: 
+// Project Name: Front-End Design for RISC-V based Chip
 // Target Devices: 
 // Tool Versions: 
-// Description: 
+// Description: RAM to be used for instruction and data memory.
 // 
 // Dependencies: 
 // 
@@ -20,7 +20,12 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module RAM(
+module RAM(clk, r_wn, address, data_in, data_out);
 
-    );
+input clk;
+input r_wn;
+input [11:0] address;
+input [31:0] data_in;
+output [31:0] data_out;
+
 endmodule
