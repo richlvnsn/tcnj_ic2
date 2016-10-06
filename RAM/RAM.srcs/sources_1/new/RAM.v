47df876401aea2267b0623ca76d80e8aef791032
DanSarnelli <sarneld1@tcnj.edu>
2016-10-05 22:20:40 -0400

    Added initial project skeleton for RAM.

---
+++ RAM/RAM.srcs/sources_1/new/RAM.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/05/2016 10:18:20 PM
+// Design Name: 
+// Module Name: RAM
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RAM(
+
+    );
+endmodule
