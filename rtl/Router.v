0c2f3360bbd679880442b79769f2e44d10db7c29
DanSarnelli <sarneld1@tcnj.edu>
2016-11-01 16:43:19 -0400

    Split Router IOs for core into instruction and data memory. Also added an input for reset.

--- rtl/Router.v
+++ rtl/Router.v
@@ -21,18 +21,20 @@
 
 
 module Router(
-    clk,
+    clk, reset,
     // SPI Loader IOs
     spi_hready, spi_hrest, spi_hrdata, spi_haddr, spi_hwrite, spi_hsize, spi_hburst, spi_hmastlock, spi_hprot, spi_htrans, spi_hwdata,
-    // RISC-V Core IOs
-    imem_hready, imem_hresp, dmem_hready, dmem_hresp, imem_hrdata, dmem_hrdata, imem_haddr, imem_hwrite, imem_hsize, imem_hburst, imem_hmastlock, imem_hprot, imem_htrans, dmem_haddr, dmem_hwrite, dmem_hsize, dmem_hburst, dmem_hmastlock, dmem_hprot, dmem_htrans, imem_hwdata, dmem_hwdata,
+    // RISC-V Core IOs (Instruction Memory)
+    imem_hready, imem_hresp, imem_hrdata, imem_haddr, imem_hwrite, imem_hsize, imem_hburst, imem_hmastlock, imem_hprot, imem_htrans, imem_hwdata,
+    // RISC-V Core IOs (Data Memory)
+    dmem_hready, dmem_hresp, dmem_hrdata, dmem_haddr, dmem_hwrite, dmem_hsize, dmem_hburst, dmem_hmastlock, dmem_hprot, dmem_htrans, dmem_hwdata,
     // Register IOs
     reg_read, reg_write, reg_addr, reg_wben, reg_rwn,
     // RAM IOs
     inst_read, data_read, inst_write, data_write, inst_addr, data_addr, inst_rwn, data_rwn
 );
 
-input clk;
+input clk, reset;
 
 //SPI Loader IOs
 input [31:0] spi_haddr, spi_hwdata;
@@ -44,15 +46,25 @@ input spi_hwrite, spi_hmastlock;
 output [31:0] spi_hrdata;
 output spi_hready, spi_hrest;
 
-// RISC-V Core IOs
-input [31:0] imem_haddr, dmem_haddr, imem_hwdata, dmem_hwdata;
-input [3:0] imem_hprot, dmem_hprot;
-input [2:0] imem_hsize, dmem_hsize, imem_hburst, dmem_hburst;
-input [1:0] imem_htrans, dmem_htrans;
-input imem_hwrite, dmem_hwrite, imem_hmastlock, dmem_hmastlock;
+// RISC-V Core IOs (Instruction Memory)
+input [31:0] imem_haddr, imem_hwdata;
+input [3:0] imem_hprot;
+input [2:0] imem_hsize, imem_hburst;
+input [1:0] imem_htrans;
+input imem_hwrite, imem_hmastlock;
 
-output [31:0] imem_hrdata, dmem_hrdata;
-output imem_hready, dmem_hready, imem_hresp, dmem_hresp;
+output [31:0] imem_hrdata;
+output imem_hready, imem_hresp;
+
+// RISC-V Core IOs (Data Memory)
+input [31:0] dmem_haddr, dmem_hwdata;
+input [3:0] dmem_hprot;
+input [2:0] dmem_hsize, dmem_hburst;
+input [1:0] dmem_htrans;
+input dmem_hwrite, dmem_hmastlock;
+
+output [31:0] dmem_hrdata;
+output dmem_hready, dmem_hresp;
 
 // Register IOs
 input [31:0] reg_read;
