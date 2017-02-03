28e683dd0f011f1d774cd07487e7d2655b24db32
Richard Levenson <richlvnsn@gmail.com>
2017-02-02 23:09:38 -0500

    Bug fixes in chip.v following initial integration testing

--- rtl/chip.v
+++ rtl/chip.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // The College of New Jersey
-// Engineer: Dan Sarnelli
+// Engineer: Dan Sarnelli, Richard Levenson
 // 
 // Create Date: 11/16/2016 04:58:54 PM
 // Module Name: chip
@@ -35,18 +35,18 @@ wire imem_hwrite;
 wire [2:0] imem_hsize;
 wire [2:0] imem_hburst;
 wire imem_hmastlock;
-wire [3:0] imem_hrpot;
+wire [3:0] imem_hprot;
 wire [1:0] imem_htrans;
 wire [31:0] imem_hwdata;
 wire [31:0] imem_hrdata;
 wire imem_hready;
-wire dmem_hresp;
+wire imem_hresp;
 wire [31:0] dmem_haddr;
 wire dmem_hwrite;
 wire [2:0] dmem_hsize;
 wire [2:0] dmem_hburst;
 wire dmem_hmastlock;
-wire [3:0] dmem_hrpot;
+wire [3:0] dmem_hprot;
 wire [1:0] dmem_htrans;
 wire [31:0] dmem_hwdata;
 wire [31:0] dmem_hrdata;
@@ -119,19 +119,19 @@ vscale_core core(   .clk(clk),
                     .imem_hsize(imem_hsize),
                     .imem_hburst(imem_hburt),
                     .imem_hmastlock(imem_hmastlock),
-                    .imem_hrpot(imem_hrpot),
+                    .imem_hprot(imem_hprot),
                     .imem_htrans(imem_htrans),
                     .imem_hwdata(imem_hwdata),
                     .imem_hrdata(imem_hrdata),
                     .imem_hready(imem_hready),
+                    .imem_hresp(imem_hresp),
                     // Router (Data Memory AHB Lines)
-                    .dmem_hresp(dmem_hresp),
                     .dmem_haddr(dmem_haddr),
                     .dmem_hwrite(dmem_hwrite),
                     .dmem_hsize(dmem_hsize),
                     .dmem_hburst(dmem_hburst),
                     .dmem_hmastlock(dmem_hmastlock),
-                    .dmem_hrpot(dmem_hrpot),
+                    .dmem_hprot(dmem_hprot),
                     .dmem_htrans(dmem_htrans),
                     .dmem_hwdata(dmem_hwdata),
                     .dmem_hrdata(dmem_hrdata),
@@ -223,58 +223,58 @@ assign iram0_rwn = inst_rwn || inst_wben[0];
 RAM iram0(  .clk(clk), 
             .r_wn(iram0_rwn),
             .address(inst_addr),
-            .data_in(inst_write),
-            .data_out(inst_read)
+            .data_in(inst_write[7:0]),
+            .data_out(inst_read[7:0])
 );
 assign iram1_rwn = inst_rwn || inst_wben[1];
 RAM iram1(  .clk(clk), 
             .r_wn(iram1_rwn),
             .address(inst_addr),
-            .data_in(inst_write),
-            .data_out(inst_read)
+            .data_in(inst_write[15:8]),
+            .data_out(inst_read[15:8])
 );
 assign iram2_rwn = inst_rwn || inst_wben[2];
 RAM iram2(  .clk(clk), 
             .r_wn(iram2_rwn),
             .address(inst_addr),
-            .data_in(inst_write),
-            .data_out(inst_read)
+            .data_in(inst_write[23:16]),
+            .data_out(inst_read[23:16])
 );
 assign iram3_rwn = inst_rwn || inst_wben[3];
 RAM iram3(  .clk(clk), 
             .r_wn(iram3_rwn),
             .address(inst_addr),
-            .data_in(inst_write),
-            .data_out(inst_read)
+            .data_in(inst_write[31:24]),
+            .data_out(inst_read[31:24])
 );
 // Data RAM
 assign dram0_rwn = data_rwn || data_wben[0];
 RAM dram0(  .clk(clk), 
             .r_wn(dram0_rwn),
             .address(data_addr),
-            .data_in(data_write),
-            .data_out(data_read)
+            .data_in(data_write[7:0]),
+            .data_out(data_read[7:0])
 );
 assign dram1_rwn = data_rwn || data_wben[1];
 RAM dram1(  .clk(clk), 
             .r_wn(dram1_rwn),
             .address(data_addr),
-            .data_in(data_write),
-            .data_out(data_read)
+            .data_in(data_write[15:8]),
+            .data_out(data_read[15:8])
 );
 assign dram2_rwn = data_rwn || data_wben[2];
 RAM dram2(  .clk(clk), 
             .r_wn(dram2_rwn),
             .address(data_addr),
-            .data_in(data_write),
-            .data_out(data_read)
+            .data_in(data_write[23:16]),
+            .data_out(data_read[23:16])
 );
 assign dram3_rwn = data_rwn || data_wben[3];
 RAM dram3(  .clk(clk), 
             .r_wn(dram3_rwn),
             .address(data_addr),
-            .data_in(data_write),
-            .data_out(data_read)
+            .data_in(data_write[31:24]),
+            .data_out(data_read[31:24])
 );
 
 // Registers
