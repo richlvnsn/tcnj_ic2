6040d03433a2dcb3f0ff45dfeb7c6a19cbff0f3d
Jeremy Leon <Jeremy Leon>
2017-03-29 14:14:32 -0400

    added hex creation to makefile

--- sw/tools/spiconverter.py
+++ sw/tools/spiconverter.py
@@ -1,4 +1,3 @@
-"""
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -7,25 +6,14 @@ parser.add_argument("-o", "--output", help="name of output hex file")
 args = parser.parse_args()
 
 plain_hex = open(args.input, 'r')
-new_hex = open(args.output, 'w')"""
+new_hex = open(args.output, 'w')
 
-#plain_hex = open("constant.hex", 'r')
-new_hex = open("GPIO_SPI.hex", 'w')
-
-
-with open('GPIO.hex') as h:
-    lines = h.readlines()
+lines = plain_hex.readlines()
 
 new_hex.write("00\n20\n00\n00\n")
 
 for line in lines:
 	for x in range(14,-1,-2):
 		new_hex.write(line[x:x+2] + '\n')
-"""
-for line in lines:
-	for x in range(6,-1,-2):
-		new_hex.write(line[x:x+2] + '\n')
-	for x in range(14,7,-2):
-		new_hex.write(line[x:x+2] + '\n')
-"""
+
 new_hex.close()
